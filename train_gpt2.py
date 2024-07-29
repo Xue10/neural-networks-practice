e5480d30036dd2e2e31bbab09797e440d45fe2f9
Xue10 <xue1010@hotmail.com>
2024-07-30 07:57:49 +0800

    add finetune and continue gpt2

--- train_gpt2.py
+++ train_gpt2.py
@@ -110,34 +110,56 @@ class GPT(nn.Module):
         if targets is not None:
             loss = F.cross_entropy(logits.view(-1, logits.size(-1)), targets.view(-1))
         return logits, loss
-    
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
-with open('input.txt') as f:
-    text = f.read()
-data = text[:1000]
 
 import tiktoken
 
-enc = tiktoken.get_encoding('gpt2')
-tokens = enc.encode(data)
-B, T = 4, 32
-buf = torch.tensor(tokens[:B*T + 1]).to(device)
-x = buf[:-1].view(B, T)
-y = buf[1:].view(B, T)
+class DataLoaderLite:
+    def __init__(self, B, T):
+        self.B = B
+        self.T = T
+
+        with open('input.txt', 'r') as f:
+            text = f.read()
+        enc = tiktoken.get_encoding('gpt2')
+        tokens = enc.encode(text)
+        self.tokens = torch.tensor(tokens)
+        print(f"loaded {len(self.tokens)} tokens")
+        print(f"1 epoch = {len(self.tokens) // (B * T)} batches")
+
+        self.current_position = 0
+
+    def next_batch(self):
+        B, T = self.B, self.T
+        buf = self.tokens[self.current_position : self.current_position + B * T + 1]
+        x = (buf[:-1]).view(B, T)
+        y = (buf[1:]).view(B, T)
+
+        self.current_position += B * T
+
+        if self.current_position + (B * T + 1) > len(self.tokens):
+            self.current_position = 0
+        return x, y
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+train_loader = DataLoaderLite(B=4, T=32)
 
 
 model = GPT(Config())
 model.eval()
 model.to(device)
 
-logits, loss = model(x, y)
-print(loss)
 
-# sd = model.state_dict()
 
-# for k, v in sd.items():
-#     print(v)
-#     break
+optimizer = torch.optim.AdamW(model.parameters(), lr=3e-4)
+for i in range(50):
+    x, y = train_loader.next_batch()
+    x = x.to(device)
+    y = y.to(device)
+    optimizer.zero_grad()
+    logtis, loss = model(x, y)
+    loss.backward()
+    optimizer.step()
+    print(f"step {i}, loss: {loss.item()}")
 
 import sys; sys.exit(0)
 
